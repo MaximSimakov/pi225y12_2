3b4bdf1f6fcd8c30a862cbc5978a95fbcfb96228
ferypiple <arslan321asyanov@gmail.com>
2022-04-20 13:12:04 +0500

    change Form.jsp,Results.jsp,web.xml,Calc.java

--- src/main/java/lab4/Calc.java
+++ src/main/java/lab4/Calc.java
@@ -1,16 +1,16 @@
 package lab4;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name="Calc", urlPatterns="/Calc")
 
 public class Calc extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
 		Calc.setAsRequestAttributesAndCalculate(request);
@@ -21,18 +21,21 @@ public class Calc extends HttpServlet {
 		private final String first_calc;
 		private final String second_calc;
 		private int result;
-	private RequestCalc(String first,String second) {
+		
+		private RequestCalc(String first,String second) {
 		this.first_calc = first;
 		this.second_calc = second;
 	}
+		
 	public static RequestCalc fromRequestParameters(HttpServletRequest request) {
 		return new RequestCalc(
-		request.getParameter("length"),
-		request.getParameter("width"));
+		request.getParameter("first"),
+		request.getParameter("second"));
 	}
+	
 	public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
-		request.setAttribute("length", first_calc);
-		request.setAttribute("width", second_calc);
+		request.setAttribute("first", first_calc);
+		request.setAttribute("second", second_calc);
 		int first_try;
 		int second_try;
 		try {
